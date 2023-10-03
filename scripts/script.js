2573422b8870e1a05e738e6b63075095b21bd29c
Estorc <51833938+Estorc@users.noreply.github.com>
2023-10-03 12:10:55 +0200

    fds
    gds

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,19 @@ function parallax() {
 }
 
 
+returnHome = function (item) {
+	
+	
+		document.body.style.animation = 'disappear 1s ease-in-out';
+		document.body.style.opacity = 0;
+		
+		document.body.addEventListener("animationend", () => {
+			window.open(".","_self")
+		});
+	
+	
+}
+
 
 function phoneMenu() {
 	let phoneMenuList = document.getElementById('phoneMenuList');
