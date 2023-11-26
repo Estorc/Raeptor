424fb5b03c98b7a7c605c291670d5dd57d7f7bf6
Estorc <51833938+Estorc@users.noreply.github.com>
2023-11-26 19:18:36 +0100

    Update main.js

--- 3DSoftBodyPureJS/scripts/main.js
+++ 3DSoftBodyPureJS/scripts/main.js
@@ -630,8 +630,8 @@ const cubes = [];
 	cubes[i].color = {red:Math.random()*(255-ctx.backColor.red),green:Math.random()*(255-ctx.backColor.green),blue:Math.random()*(255-ctx.backColor.blue)};
 }*/
 
+cubes.push(new Polyhedron(0,-5,2+i/10,2,2,2,5));
 for (let i = 0; i<5; i++) {
-	cubes.push(new Polyhedron(0,-5,2+i/10,2,2,2,5));
 	cubes.push(new Cube(0+i*3,-5-Math.random()*5,0,1+Math.random()*2,1+Math.random()*7,1+Math.random()*2,2));
 	cubes[i].color = {red:Math.random()*(255-ctx.backColor.red),green:Math.random()*(255-ctx.backColor.green),blue:Math.random()*(255-ctx.backColor.blue)};
 }
